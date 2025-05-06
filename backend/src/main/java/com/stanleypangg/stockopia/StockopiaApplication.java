5220e490fe28de56eed1f2e025c7ddda701faa03
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-05 21:49:00 -0400

    initial commit

---
+++ backend/src/main/java/com/stanleypangg/stockopia/StockopiaApplication.java
@@ -0,0 +1,13 @@
+package com.stanleypangg.stockopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockopiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stanleypangg/stockopia/StockopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanleypangg.stockopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
